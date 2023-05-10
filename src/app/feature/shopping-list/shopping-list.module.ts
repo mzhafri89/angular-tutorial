d6e0d5551a9625fc38dc9ce50f325891304dfb64
zaf <mzhafri89@gmail.com>
2023-05-10 11:07:34 +0800

    chore: Generate shopping-list feature component.

---
+++ src/app/feature/shopping-list/shopping-list.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EditComponent } from './pages/edit/edit.component';
+
+
+
+@NgModule({
+  declarations: [
+    EditComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ShoppingListModule { }
