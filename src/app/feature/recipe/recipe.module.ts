6f8d0ddcc63fb8cb8292144c30b511495e15f596
zaf <mzhafri89@gmail.com>
2023-05-10 11:17:12 +0800

    ref: Restructured recipe component folder.

--- src/app/feature/recipe/recipe.module.ts
+++ src/app/feature/recipe/recipe.module.ts
@@ -1,19 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ListComponent } from './pages/list/list.component';
-import { DetailComponent } from './pages/detail/detail.component';
+import { ListComponent } from './components/list/list.component';
+import { DetailComponent } from './components/detail/detail.component';
 import { ItemComponent } from './components/item/item.component';
-
-
+import { LandingComponent } from './pages/landing/landing.component';
 
 @NgModule({
-  declarations: [
-    ListComponent,
-    DetailComponent,
-    ItemComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [ListComponent, DetailComponent, ItemComponent, LandingComponent],
+  imports: [CommonModule],
 })
-export class RecipeModule { }
+export class RecipeModule {}
