9425c677038142f743e5a875da0f4289a1bb6973
zaf <mzhafri89@gmail.com>
2023-05-11 11:05:25 +0800

    feat: Components styling.

--- src/app/feature/recipe/components/list/list.component.ts
+++ src/app/feature/recipe/components/list/list.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../../models/recipe.model';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  styleUrls: ['./list.component.css'],
 })
 export class ListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe(
+      'title',
+      'desc',
+      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505'
+    ),
+  ];
 }
--- src/app/feature/shopping-list/pages/landing/landing.component.ts
+++ src/app/feature/shopping-list/pages/landing/landing.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { Ingredient } from 'src/app/core/models/ingredient.model';
 
 @Component({
   selector: 'app-landing-shopping-list',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.css'],
 })
-export class LandingComponent {}
+export class LandingComponent {
+  ingredients: Ingredient[] = [new Ingredient('Apple', 5), new Ingredient('Tomato', 10)];
+}
