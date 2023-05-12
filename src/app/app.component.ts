010b2eeaa6dbdd5830b5190b0da18407dd1cc54b
zaf <mzhafri89@gmail.com>
2023-05-12 16:41:23 +0800

    feat: Crude navigation.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-tutorial';
+  feature = 'recipe';
+
+  handleNavigation(feature: string) {
+    this.feature = feature;
+  }
 }
