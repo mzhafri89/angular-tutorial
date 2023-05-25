7f3d9204e78e37e0b832d45e8555986999870cb5
zaf <mzhafri89@gmail.com>
2023-05-25 10:42:19 +0800

    feat: Shopping list forms.

--- src/app/feature/shopping-list/shopping-list.module.ts
+++ src/app/feature/shopping-list/shopping-list.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EditComponent } from './components/edit/edit.component';
 import { LandingComponent } from './pages/landing/landing.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [EditComponent, LandingComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   exports: [LandingComponent],
 })
 export class ShoppingListModule {}
